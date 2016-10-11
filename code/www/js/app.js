1576ff3493a5424cb0907a8e359f00274d67aa7b
Nan Ma <nan@192-168-1-131.tpgi.com.au>
2016-10-11 15:40:05 +1100

    wrapping ui and core functionality

--- code/www/js/app.js
+++ code/www/js/app.js
@@ -30,12 +30,23 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
   $stateProvider
 
 
-  // Set up an abstract state for the tabs directive:
+  // setup an abstract state for the tabs directive
   .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html',
-    controller: 'TabsCtrl'
+    controller: 'TabsCtrl',
+    // don't load the state until we've populated our User, if necessary.
+    resolve: {
+      populateSession: function(User) {
+        return User.checkSession();
+      }
+    },
+    onEnter: function($state, User){
+      User.checkSession().then(function(hasSession) {
+        if (!hasSession) $state.go('splash');
+      });
+    }
   })
 
   // Each tab has its own nav history stack:
@@ -50,6 +61,18 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
     }
   })
 
+ // splash page
+  .state('splash', {
+    url: '/',
+    templateUrl: 'templates/splash.html',
+    controller: 'SplashCtrl',
+     onEnter: function($state, User){
+      User.checkSession().then(function(hasSession) {
+        if (hasSession) $state.go('tab.discover');
+      });
+     }
+  })
+
   .state('tab.favorites', {
       url: '/favorites',
       views: {
@@ -60,7 +83,7 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
       }
     })
   // If none of the above states are matched, use this as the fallback:
-  $urlRouterProvider.otherwise('/tab/discover');
+  $urlRouterProvider.otherwise('/');
 
 })
 
